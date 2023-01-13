c90eba7c8500dfbf33ff72e040a0376a0f90ae36
Dzim4ik1 <dmitriy.vozhzhov@gmail.com>
2023-01-13 15:05:29 +0200

    Updates

---
+++ precache-manifest.3cf2e7a8b6058498d2a68cd9a2fb2685.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "fda778ced095802d8de0",
+    "url": "/Portfoli023/static/css/main.37e23e9e.chunk.css"
+  },
+  {
+    "revision": "fda778ced095802d8de0",
+    "url": "/Portfoli023/static/js/main.fda778ce.chunk.js"
+  },
+  {
+    "revision": "8c8bfd8947acae143ed6",
+    "url": "/Portfoli023/static/js/1.8c8bfd89.chunk.js"
+  },
+  {
+    "revision": "12a2eca582cfda70a270",
+    "url": "/Portfoli023/static/js/runtime~main.12a2eca5.js"
+  },
+  {
+    "revision": "6f72eccfca54a7a2bc11c5a40bd8ca78",
+    "url": "/Portfoli023/static/media/02-big.6f72eccf.jpg"
+  },
+  {
+    "revision": "ccaa7d239372739f1894437fc06843a9",
+    "url": "/Portfoli023/static/media/sun.ccaa7d23.svg"
+  },
+  {
+    "revision": "c0e1ca3d6c5df5bd7dbad48c0f1d5cb2",
+    "url": "/Portfoli023/static/media/moon.c0e1ca3d.svg"
+  },
+  {
+    "revision": "213bedfaa9aba9dd10d23eff4022f0cd",
+    "url": "/Portfoli023/static/media/telegram.213bedfa.svg"
+  },
+  {
+    "revision": "c5787617d399bcbf94789945a15f86b3",
+    "url": "/Portfoli023/static/media/gitHub.c5787617.svg"
+  },
+  {
+    "revision": "336dd870b18a0cbbcc0498151e977c80",
+    "url": "/Portfoli023/static/media/instagram.336dd870.svg"
+  },
+  {
+    "revision": "a78d33899d7ec16a5602bef270c93c05",
+    "url": "/Portfoli023/static/media/linkedIn.a78d3389.svg"
+  },
+  {
+    "revision": "c365f280f591bb9dd12631a832e4a731",
+    "url": "/Portfoli023/static/media/01.c365f280.jpg"
+  },
+  {
+    "revision": "3c386d13e93589c59d9e950af2dee058",
+    "url": "/Portfoli023/static/media/01-big.3c386d13.jpg"
+  },
+  {
+    "revision": "a972ed879af11857684c69879e8e2cba",
+    "url": "/Portfoli023/static/media/02.a972ed87.jpg"
+  },
+  {
+    "revision": "1e0f3889f3a32f7fce5b5b342c6fd5ba",
+    "url": "/Portfoli023/static/media/03.1e0f3889.jpg"
+  },
+  {
+    "revision": "fe240335043b611a00b0e13f3055cc82",
+    "url": "/Portfoli023/static/media/03-big.fe240335.jpg"
+  },
+  {
+    "revision": "3e8ef998e66dce4af9c6ad76d61ac553",
+    "url": "/Portfoli023/static/media/04.3e8ef998.jpg"
+  },
+  {
+    "revision": "37032f814e5b7af9758fa4f26fd03577",
+    "url": "/Portfoli023/static/media/04-big.37032f81.jpg"
+  },
+  {
+    "revision": "a7643e6d2552d31222040fc3b408b2e4",
+    "url": "/Portfoli023/static/media/gitHub-black.a7643e6d.svg"
+  },
+  {
+    "revision": "6f3e199351c1a9e63fa72c93d9a8386f",
+    "url": "/Portfoli023/static/media/header-bg.6f3e1993.png"
+  },
+  {
+    "revision": "855a95d3213f3127d79f268018dd5c5e",
+    "url": "/Portfoli023/index.html"
+  }
+];
